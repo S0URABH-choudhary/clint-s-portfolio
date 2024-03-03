74c8b4d733aee1ef40eec2c20e5b6ea31fd4400a
S0URABH_choudhary <subh7898765@gmail.com>
2024-03-03 22:44:26 +0530

    uploding project

---
+++ script.js
@@ -0,0 +1,7 @@
+var typingeffect = new Typed("#text",{
+    strings : ["freelancer","designer","digital marketer"],
+    loop : true,
+    typespeed : 100,
+    backspeed : 80,
+    backDelay : 1000,
+})
