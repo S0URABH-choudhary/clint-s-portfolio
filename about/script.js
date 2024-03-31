3c89df4ace41264e4a12e2e69cb841a72b9af39f
S0URABH_choudhary <subh7898765@gmail.com>
2024-03-31 08:07:53 +0530

    done

--- about/script.js
+++ about/script.js
@@ -2,3 +2,18 @@ function hello(){
     alert("no project done yet");
 
 }
+let button = document.querySelector("#btn")
+let image = document.querySelector("#photo")
+let flag = 0
+button.addEventListener("click",function(){
+    if(flag == 0){
+        image.style.width = "60%"
+        image.style.border = "3px solid black"
+        flag = 1
+    }else{
+        image.style.width = "0"
+        image.style.border = "0px"
+        flag = 0
+    }
+})
+
